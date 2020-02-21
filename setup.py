a27855a0b1633afc54b00e47605be2eeee6c4d98
Praneeth <praneethbedapudi@gmail.com>
2020-02-21 17:53:57 +0530

    use opencv headless

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ REQUIRED = [
     'pydload',
     'tensorflow==1.14.0',
     'keras==2.2.4',
-    'opencv-python==4.2.0.32',
+    'opencv-python-headless==4.2.0.32',
     'scipy==1.4.1',
     'Shapely==1.7.0'
 ]
